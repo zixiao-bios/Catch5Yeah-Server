b28703da9d819b36f21985a0474321941e32c6c7
zixiao_bios <zixiao_bios@outlook.com>
2022-02-09 17:35:27 +0800

    get request params demo

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
-const http = require("http");
+const http = require('http');
 
 const port = 7977;
 
 const server = http.createServer((req, res) => {
+    console.log("request method: " + req.method);
+    console.log("url: " + req.url);
+    const myURL = new URL(req.url, 'https://example.org/');
+    const param = myURL.searchParams.get("opt");
+
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain')
+    res.write("your opt: " + param + "\n");
     res.end('Hello World\n');
 });
 
