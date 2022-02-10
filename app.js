8c515bb231d61374c11ba8d2c64bee631fc914ea
zixiao_bios <zixiao_bios@outlook.com>
2022-02-10 17:37:39 +0800

    get datetime

--- app.js
+++ app.js
@@ -4,15 +4,24 @@ const verify = require('./verify');
 const port = 7977;
 
 const server = http.createServer((req, res) => {
-    console.log("request method: " + req.method);
-    console.log("url: " + req.url);
     const params = new URL(req.url, 'https://example.org/').searchParams;
     if (params.get("id") === verify.id && params.get('password') === verify.password) {
-        const opt = params.get("opt");
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/plain');
-        res.write("your opt: " + opt + "\n");
-        res.end('Hello World\n');
+
+        switch (params.get("opt")) {
+            case "datetime":
+                const datetime = new Date().toLocaleString('chinese', {hour12: false});
+                res.write(datetime);
+                break;
+            case "num":
+                break;
+            case "message":
+                break;
+            default:
+                res.write("undefined option");
+        }
+        res.end();
     } else {
         res.statusCode = 401;
         res.end();
