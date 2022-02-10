2f6649695b37e6fc6331dfb50a06a086b56bcb25
zixiao_bios <zixiao_bios@outlook.com>
2022-02-10 13:38:26 +0800

    add verification

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 const http = require('http');
+const verify = require('./verify');
 
 const port = 7977;
 
 const server = http.createServer((req, res) => {
     console.log("request method: " + req.method);
     console.log("url: " + req.url);
-    const myURL = new URL(req.url, 'https://example.org/');
-    const param = myURL.searchParams.get("opt");
-
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain')
-    res.write("your opt: " + param + "\n");
-    res.end('Hello World\n');
+    const params = new URL(req.url, 'https://example.org/').searchParams;
+    if (params.get("id") === verify.id && params.get('password') === verify.password) {
+        const opt = params.get("opt");
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.write("your opt: " + opt + "\n");
+        res.end('Hello World\n');
+    } else {
+        res.statusCode = 401;
+        res.end();
+    }
 });
 
 server.listen(port, () => {
