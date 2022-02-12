0ff11a3b873ad850127427ca898c9bbaadad093d
zixiao_bios <zixiao_bios@outlook.com>
2022-02-13 03:40:37 +0800

    test

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const verify = require('./verify');
 const port = 7977;
 
 const server = http.createServer((req, res) => {
+    console.log(req.url);
     const params = new URL(req.url, 'https://example.org/').searchParams;
     if (params.get("id") === verify.id && params.get('password') === verify.password) {
         res.statusCode = 200;
