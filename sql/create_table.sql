dfa011b31230a69bb528ede5c47b5d4fa11f940c
zixiao_bios <zixiao_bios@outlook.com>
2022-06-30 00:08:35 +0800

    update .ignore

--- sql/create_table.sql
+++
@@ -1,8 +0,0 @@
-create table `requests`
-(
-    `id`   int unsigned auto_increment,
-    `time` datetime    not null,
-    `ip`   varchar(15) not null,
-    `type` varchar(10) not null,
-    primary key (`id`)
-);
