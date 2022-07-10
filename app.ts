ce5bfc9c2bd69ea9605fc88935e219800bbdcd29
zixiao_bios <zixiao_bios@outlook.com>
2022-07-11 02:00:53 +0800

    switch to typescript

--- app.ts
+++ app.ts
@@ -1,13 +1,13 @@
-const http = require('http')
-const moment = require('moment')
-const config = require('./config.json')
+import moment from 'moment'
+import http from 'http'
+import config from './config.json'
 
 const port = 7977
 
 const server = http.createServer((req, res) => {
     console.log(req.url)
     console.log("client IP: " + req.socket.remoteAddress)
-    const params = new URL(req.url, 'https://example.org/').searchParams
+    const params = new URL(req.url as string, 'https://example.org/').searchParams
     if (params.get("id") === config.api_verify.id && params.get('password') === config.api_verify.password) {
         res.statusCode = 200
         res.setHeader('Content-Type', 'text/plain')
